7eb3df67fa23cc67e513e42864f2efcc62c57fb6
Weiller Carvalho <engweiller@hotmail.com>
2023-03-22 23:54:42 -0300

    feat: Making SignIn/SignOut button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,48 @@
-import { FC } from 'react'
+import { getServerSession } from "next-auth";
+import Link from "next/link";
+import { FC } from "react";
+import { buttonVariants } from "@/ui/Button";
 
-interface NavbarProps {
-  
-}
+interface NavbarProps {}
 
-const Navbar: FC<NavbarProps> = ({}) => {
-  return <div>Navbar</div>
-}
+const Navbar = async ({}) => {
+  const session = await getServerSession();
 
-export default Navbar
+  return (
+    <div className="fixed backdrop-blur-sm bg-white/75 dark:bg-slate-900 z-50 top-0 left-0 right-0 h-20 border-b border-slate-300 dark:bolder-slate-700 shadow-sm flex items-center justify-between">
+      <div className="container max-w-7xl mx-auto w-full flex justify-between items-center">
+        <Link href="/" className={buttonVariants({ variant: "link" })}>
+          Text Similarity 1.0
+        </Link>
+
+        <div className="md:hidden">
+          <ThemeToogle />
+        </div>
+        <div className="hidden md:flex gap-4">
+          <ThemeToogle />
+          <Link
+            href="/documentation"
+            className={buttonVariants({ variant: "ghost" })}
+          >
+            Documentation
+          </Link>
+          {session ? (
+            <>
+              <Link
+                href="/dashboard"
+                className={buttonVariants({ variant: "ghost" })}
+              >
+                Dashboard
+              </Link>
+              <SignOutButton />
+            </>
+          ) : (
+            <SignInButton />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,55 @@
+import { ButtonHTMLAttributes, FC, forwardRef } from "react";
+import { VariantProps, cva } from "class-variance-authority";
+import { cn } from "@/lib/utils";
+import { Loader2 } from "lucide-react";
+
+interface ButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  isLoading?: boolean;
+}
+
+export const buttonVariants = cva(
+  "active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-color focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disable:opacity-50 dark:focus:ring-slate-400 disable:pointer-events-none dark:focus:ring-offset-slate-900",
+  {
+    variants: {
+      variant: {
+        default:
+          "bg-slate-900 text-white hover:bg-slate-800 dark:bg-slate-200 dark:text-slate-900 dark:hover:bg-slate-100",
+        outline:
+          "bg-slate-900 text-white hover:bg-slate-800 dark:bg-slate-200 dark:text-slate-900 dark:hover:bg-slate-100 border border-slate-200 hover:bg-slate-100 dark:border-slate-700",
+        ghost:
+          "bg-transparent hover:bg-slate-100 dark:hover:bg-slate-800 dark:text-slate-400 data-[state=open]:bg-transparent dark:data-[state=open]:bg-transparent",
+        link: "bg-transparent dark:bg-transparent underline-offset-4 hover:underline text-slate-900 dark:text-slate-100 hover:bg-transparent dark:hover:bg-transparent",
+      },
+      size: {
+        default: "h-10 py-2 px-4",
+        sm: "h-9 px-2 rounded-md",
+        lg: "h-11 px-8 rounded-md",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
+
+// eslint-disable-next-line react/display-name
+const Button: FC<ButtonProps> = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, children, variant, isLoading, size, ...props }, ref) => {
+    return (
+      <button
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        disabled={isLoading}
+        {...props}
+      >
+        {isLoading ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : null}
+        {children}
+      </button>
+    );
+  }
+);
+Button.displayName = "Button";
+export default Button;
