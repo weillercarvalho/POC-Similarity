e2242a37b2f03a207715299e4a700f93016a81ac
Weiller Carvalho <engweiller@hotmail.com>
2023-03-23 22:05:46 -0300

    style: Page.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,29 @@
-import '@/styles/globals.css'
-import { Inter } from 'next/font/google'
-import { cn } from '@/lib/utils';
-import Providers from '@/components/Providers';
-import Navbar from '@/components/Navbar';
-const inter = Inter({subsets: ['latin']});
+import "@/styles/globals.css";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+import Providers from "@/components/Providers";
+import Navbar from "@/components/Navbar";
+import { Toaster } from "@/components/ui/Toast";
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={cn("bg-white text-slate-900 antialiased", inter.className)}>
-      <body className='min-h-screen bg-slate-50 dark:bg-slate-900 antialiased'>
+    <html
+      lang="en"
+      className={cn("bg-white text-slate-900 antialiased", inter.className)}
+    >
+      <body className="min-h-screen bg-slate-50 dark:bg-slate-900 antialiased">
         <Providers>
           {children}
+          <Toaster position="bottom-right" />
           <Navbar />
         </Providers>
         <div className="h-40 md:hidden"></div>
-        </body>
+      </body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
-import { Inter } from 'next/font/google'
-import Paragraph from '@/components/ui/Paragraph'
+import { Inter } from "next/font/google";
+import LargeHeading from "@/components/ui/LargeHeading";
+import type { Metadata } from "next";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Similarity API",
+  authors: [{ name: "Weiller" }, { url: "http://weillercarvalho.com" }],
+};
 
 export default function Home() {
   return (
-    <main className='bg-red-500'>
-      <Paragraph size="sm">Test</Paragraph>
-    </main>
-  )
+    <div className="relative h-screen flex items-center justify-center overflow-x-hidden">
+      <div className="container pt-32 max-w-7xl mx-auto w-full h-full">
+        <div className="h-full gap-6 flex flex-col justify-start lg:justify-center items-center lg:items-start">
+          <LargeHeading></LargeHeading>
+        </div>
+      </div>
+    </div>
+  );
 }
